daac628bf86b6259c0912a9ae0333e8aa77f551d
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2025-01-22 19:33:40 +0400

    mob

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -38,62 +38,52 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-document.querySelector('#checkout-btn-1-1').addEventListener('click', function() {
-  // Убираем класс 'active' и добавляем класс 'hidden' для #checkout-forms-content-1
-  const content1 = document.querySelector('#checkout-forms-content-1');
-  content1.classList.remove('active');
-  content1.classList.add('hidden');
 
-  // Убираем класс 'hidden' и добавляем класс 'active' для #checkout-forms-content-2
-  const content2 = document.querySelector('#checkout-forms-content-2');
-  content2.classList.remove('hidden');
-  content2.classList.add('active');
+function toggleContent(hideSelector, showSelector) {
+  const hideElement = document.querySelector(hideSelector);
+  const showElement = document.querySelector(showSelector);
 
-  // Прокручиваем страницу наверх
-  window.scrollTo({
+  if (hideElement && showElement) {
+    hideElement.classList.remove('active');
+    hideElement.classList.add('hidden');
+
+    showElement.classList.remove('hidden');
+    showElement.classList.add('active');
+
+    // Прокручиваем страницу наверх
+    window.scrollTo({
       top: 0,
-      behavior: 'smooth' // Плавный скроллинг
-  });
+      behavior: 'smooth',
+    });
+  }
+}
+
+
+document.addEventListener('click', (event) => {
+  if (event.target.matches('.checkout-form-1-btn-mob')) {
+    const content1 = document.querySelector('#checkout-forms-content-1');
+    const content2Inner = document.querySelector('.checkout-forms-content-2-inner');
+
+    if (content1.classList.contains('active')) {
+      toggleContent('#checkout-forms-content-1', '#checkout-forms-content-2');
+      toggleContent(null, '.checkout-forms-content-2-inner');
+    } else if (content2Inner.classList.contains('active')) {
+      toggleContent('.checkout-forms-content-2-inner', '.checkout-forms-content-3-inner');
+    }
+  }
+
+  if (event.target.matches('#checkout-btn-1-1')) {
+    toggleContent('#checkout-forms-content-1', '#checkout-forms-content-2');
+  }
+
+  if (event.target.matches('.checkout-forms-content-2-info-btn')) {
+    toggleContent('#checkout-forms-content-2', '#checkout-forms-content-1');
+  }
+
+  if (event.target.matches('.checkout-forms-content-2-inner-btn')) {
+    toggleContent('.checkout-forms-content-2-inner', '.checkout-forms-content-3-inner');
+  }
 });
-
-// Добавляем обработчик для кнопки с классом .checkout-forms-content-2-info-btn
-document.querySelector('.checkout-forms-content-2-info-btn').addEventListener('click', function() {
-  // Убираем класс 'active' и добавляем класс 'hidden' для #checkout-forms-content-2
-  const content2 = document.querySelector('#checkout-forms-content-2');
-  content2.classList.remove('active');
-  content2.classList.add('hidden');
-
-  // Убираем класс 'hidden' и добавляем класс 'active' для #checkout-forms-content-1
-  const content1 = document.querySelector('#checkout-forms-content-1');
-  content1.classList.remove('hidden');
-  content1.classList.add('active');
-
-  // Прокручиваем страницу наверх
-  window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // Плавный скроллинг
-  });
-});
-
-// Добавляем обработчик для кнопки с классом .checkout-forms-content-2-inner-btn
-document.querySelector('.checkout-forms-content-2-inner-btn').addEventListener('click', function() {
-  // Убираем класс 'active' и добавляем класс 'hidden' для #checkout-forms-content-2-inner
-  const content2Inner = document.querySelector('.checkout-forms-content-2-inner');
-  content2Inner.classList.remove('active');
-  content2Inner.classList.add('hidden');
-
-  // Убираем класс 'hidden' и добавляем класс 'active' для #checkout-forms-content-3-inner
-  const content3Inner = document.querySelector('.checkout-forms-content-3-inner');
-  content3Inner.classList.remove('hidden');
-  content3Inner.classList.add('active');
-
-  // Прокручиваем страницу наверх
-  window.scrollTo({
-      top: 0,
-      behavior: 'smooth' // Плавный скроллинг
-  });
-});
-
 
 
 function selectRadio(id, element) {
